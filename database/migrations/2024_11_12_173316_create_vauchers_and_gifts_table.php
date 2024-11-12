97888766198e518a2add41f5007b6e829f680e54
Nick Kharadze <vscodeart@gmail.com>
2024-11-12 21:56:02 +0400

    Vouchers and Offers  page

---
+++ database/migrations/2024_11_12_173316_create_vauchers_and_gifts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vauchers_and_gifts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('vauchers_category_id');
+            $table->longText('content')->nullable();
+            $table->string('image')->nullable();
+            $table->foreign('vauchers_category_id')->references('id')->on('vauchers_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vauchers_and_gifts');
+    }
+};
