9dc5713e5822ab151d13081d583ff4274196c948
Nick Kharadze <vscodeart@gmail.com>
2024-11-14 02:48:01 +0400

    back: add whoweare migration

--- database/migrations/2024_11_12_181853_create_who_we_ares_table.php
+++ database/migrations/2024_11_12_181853_create_who_we_ares_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('who_we_ares', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable();
+
             $table->string('name');
             $table->longText('content')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
